56552a4cdc02065b9eb646333d7f5521473b9384
James <qjaymce37@gmail.com>
2025-10-30 22:43:51 +0000

    add admin.py

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,25 @@
+# bookshelf/admin.py
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # columns shown in the list page
+    list_display = ("title", "author", "publication_year")
+
+    # quick filters in the right sidebar
+    list_filter = ("publication_year", "author")
+
+    # search box - searches these fields (partial matches)
+    search_fields = ("title", "author")
+
+    # make title the clickable link to the change page
+    list_display_links = ("title",)
+
+    # allow inline editing of publication_year in the list view
+    # (note: list_editable cannot include the same field as list_display_links)
+    list_editable = ("publication_year",)
+
+    # optional conveniences
+    ordering = ("title",)
+    list_per_page = 20
