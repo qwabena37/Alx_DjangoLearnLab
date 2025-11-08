d7919e22c0d56dc234153cffd01ce91eb6ba6928
James <qjaymce37@gmail.com>
2025-11-08 00:36:51 +0000

    modify views.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,6 +3,9 @@ from django.contrib.auth.views import LoginView, LogoutView
 from . import views  # import your views module
 
 urlpatterns = [
+    path('admin-view/', views.admin_view, name='admin_view'),
+    path('librarian-view/', views.librarian_view, name='librarian_view'),
+    path('member-view/', views.member_view, name='member_view'),
     # Register view (function-based)
     path('register/', views.register_view, name='register'),
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 from .forms import RegisterForm
+from .models import UserProfile
+from django.contrib.auth.decorators import user_passes_test
 
 
 
@@ -89,3 +91,26 @@ def register_view(request):
         form = UserCreationForm()  # <-- instantiate empty form for GET request
 
     return render(request, 'relationship_app/register.html', {'form': form})
+
+
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
