9f673ab4e94d4ea0dea9499f3d9c050865a5cbb8
James <qjaymce37@gmail.com>
2025-11-16 17:22:19 +0000

    add decorator to bookshelf/views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
+from .models import Article
+
+@permission_required('yourapp.can_view', raise_exception=True)
+def article_list(request):
+    articles = Article.objects.all()
+    return render(request, "articles/list.html", {"articles": articles})
+
+@permission_required('yourapp.can_create', raise_exception=True)
+def article_create(request):
+    if request.method == "POST":
+        # Create logic here...
+        return redirect("article_list")
+    return render(request, "articles/create.html")
+
+@permission_required('yourapp.can_edit', raise_exception=True)
+def article_edit(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == "POST":
+        # Edit logic here...
+        return redirect("article_list")
+    return render(request, "articles/edit.html", {"article": article})
+
+@permission_required('yourapp.can_delete', raise_exception=True)
+def article_delete(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    article.delete()
+    return redirect("article_list")
 
 # Create your views here.
