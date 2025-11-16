c4929129b34551e803d593b14f58ea21affd0edc
James <qjaymce37@gmail.com>
2025-11-16 17:27:09 +0000

    add book_list to bookshelf/views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Article
+from .models import Book  # Import your Book model
 
 @permission_required('yourapp.can_view', raise_exception=True)
 def article_list(request):
@@ -28,5 +29,8 @@ def article_delete(request, pk):
     article = get_object_or_404(Article, pk=pk)
     article.delete()
     return redirect("article_list")
-
+def book_list(request):
+    books = Book.objects.all()  # Fetch all books
+    context = {'books': books}
+    return render(request, 'bookshelf/book_list.html', context)
 # Create your views here.
