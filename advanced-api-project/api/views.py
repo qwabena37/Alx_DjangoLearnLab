d62dd99ea8044b2a1fde17107e39a172576e1ceb
James <qjaymce37@gmail.com>
2025-11-28 10:04:28 +0000

    implement generics in api/views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookListView(generics.ListAPIView):
+    """
+    Retrieves a list of all Book instances.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookDetailView(generics.RetrieveAPIView):
+    """
+    Retrieves a single Book instance by its primary key.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookCreateView(generics.CreateAPIView):
+    """
+    Creates a new Book instance.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookUpdateView(generics.UpdateAPIView):
+    """
+    Updates an existing Book instance.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookDeleteView(generics.DestroyAPIView):
+    """
+    Deletes a Book instance.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
