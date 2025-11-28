4933be4d4cf43a969c48629fefce5748a58e5242
James <qjaymce37@gmail.com>
2025-11-28 09:51:37 +0000

    define data models in api/models.py

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    """
+    Represents a book author.
+    Each author can have multiple related Book objects.
+    """
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    """
+    Represents a book written by an Author.
+    Links to Author via a foreign key (one-to-many).
+    """
+    title = models.CharField(max_length=200)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(
+        Author, related_name='books', on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.title} ({self.publication_year})"
+
