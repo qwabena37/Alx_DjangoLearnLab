aecff18690b8d17a94b7524e940339ac7e7a8472
James <qjaymce37@gmail.com>
2025-12-06 17:29:45 +0000

    add blog/signals.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+# blog/signals.py
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -9,3 +13,21 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
+
+    def get_absolute_url(self):
+        return reverse("blog:profile")  # adjust namespace/name as needed
+    
+@receiver(post_save, sender=User)
+def create_or_update_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    else:
+        instance.profile.save()
---
+++ django_blog/blog/signals.py
@@ -0,0 +1,8 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, created, **kwargs):
+    # temporary placeholder to avoid errors
+    pass
