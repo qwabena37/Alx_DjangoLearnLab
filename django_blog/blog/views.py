dbb8f51ea03b2698cbc7e5ad7abd22ce171e9b5b
James <qjaymce37@gmail.com>
2025-12-06 21:04:54 +0000

    add folders

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
-# Create your views here.
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = RegisterForm()
+    return render(request, "register.html", {"form": form})
+
+
+@login_required
+def profile_view(request):
+    if request.method == "POST":
+        request.user.email = request.POST.get("email", request.user.email)
+        request.user.save()
+    return render(request, "profile.html")
