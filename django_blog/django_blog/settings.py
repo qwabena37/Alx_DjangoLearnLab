9fdefd0467708a60f4efd44b458f40d2d573c210
James <qjaymce37@gmail.com>
2025-12-06 17:09:15 +0000

    connect postgreSQL/bloguser

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blogdb',
+        'USER': 'bloguser',
+        'PASSWORD': 'james123.',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
