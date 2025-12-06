cbd48b721da70de096d93b5f629bd77e1a054753
James <qjaymce37@gmail.com>
2025-12-06 17:34:11 +0000

    add blog/views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+# blog/views.py
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import views as auth_views
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
-# Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, "Account created successfully. You can log in now.")
+            return redirect("blog:login")
+    else:
+        form = UserRegisterForm()
+    return render(request, "blog/register.html", {"form": form})
+
+@login_required
+def profile(request):
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, "Your profile has been updated.")
+            return redirect("blog:profile")
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {"u_form": u_form, "p_form": p_form}
+    return render(request, "blog/profile.html", context)
+
