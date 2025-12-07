3c7cb8a86e1aaefc055e66f44bb011b90f4e97a9
James <qjaymce37@gmail.com>
2025-12-07 17:08:40 +0000

    link postgreSQL

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blogdb',
+        'USER': 'postgres',
+        'PASSWORD': 'Kwabena123.',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
