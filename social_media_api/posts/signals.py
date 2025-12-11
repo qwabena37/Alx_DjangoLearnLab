fa961ac2b2fc57924b8edeef3b75715cc28d3cdb
James <qjaymce37@gmail.com>
2025-12-11 20:48:55 +0000

    add comment notification in posts/signals.py

--- social_media_api/posts/signals.py
+++ social_media_api/posts/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Like
 from notifications.utils import create_notification
+from .models import Comment
 
 @receiver(post_save, sender=Like)
 def like_created(sender, instance, created, **kwargs):
@@ -10,3 +11,12 @@ def like_created(sender, instance, created, **kwargs):
         actor = instance.user
         recipient = post.author
         create_notification(recipient=recipient, actor=actor, verb='liked', target=post)
+
+
+@receiver(post_save, sender=Comment)
+def comment_created(sender, instance, created, **kwargs):
+    if created:
+        post = instance.post
+        actor = instance.author
+        recipient = post.author
+        create_notification(recipient=recipient, actor=actor, verb='commented', target=instance)
