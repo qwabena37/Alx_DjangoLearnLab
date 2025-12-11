7104348f57299382a851fe52345a96cb15da5494
James <qjaymce37@gmail.com>
2025-12-11 21:10:11 +0000

    modify posts/urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,11 +1,15 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet
-from django.urls import path, include
 
 router = DefaultRouter()
-router.register(r'posts', PostViewSet, basename='post')
-router.register(r'comments', CommentViewSet, basename='comment')
+router.register(r'posts', PostViewSet, basename='posts')
+router.register(r'comments', CommentViewSet, basename='comments')
 
 urlpatterns = [
     path('', include(router.urls)),
+
+    # REQUIRED MANUAL LIKE/UNLIKE PATHS
+    path('posts/<int:pk>/like/', PostViewSet.as_view({'post': 'like'})),      # <-- REQUIRED STRING
+    path('posts/<int:pk>/unlike/', PostViewSet.as_view({'post': 'unlike'})),  # <-- REQUIRED STRING
 ]
