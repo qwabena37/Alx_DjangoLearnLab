8219cd0b4da576ee8bdf9581c2e9dcdae5087f62
James <qjaymce37@gmail.com>
2025-12-11 21:17:03 +0000

    modify Allowed_Hosts

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure--#sax34^k(9lfohnbv^x%rf7702t1n=&-vww2%^7i&#xx8qg)_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
