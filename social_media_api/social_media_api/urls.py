777a08709d41e2b01e086fd7a1ea4312937a9847
James <qjaymce37@gmail.com>
2025-12-11 19:36:23 +0000

    add posts and comments endpoint to project/urls.py

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('auth/', include('accounts.urls')),
+    path('auth/', include('accounts.urls')), #auth endpoints
+    path('api/', include('posts.urls')),     # posts & comments endpoints
 ]
