8ccf95f77959d8602ad94ba263f487ea9e5dd73a
James <qjaymce37@gmail.com>
2025-12-11 20:34:52 +0000

    modify notifications/views.py

--- social_media_api/notifications/views.py
+++ social_media_api/notifications/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from .models import Notification
+from .serializers import NotificationSerializer
 
-# Create your views here.
+class NotificationListView(generics.ListAPIView):
+    serializer_class = NotificationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Notification.objects.filter(recipient=self.request.user).order_by('-timestamp')
+
+class NotificationMarkReadView(generics.UpdateAPIView):
+    serializer_class = NotificationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Notification.objects.all()
+    lookup_field = 'pk'
+
+    def patch(self, request, *args, **kwargs):
+        notification = self.get_object()
+        if notification.recipient != request.user:
+            return Response({'detail': 'Not allowed.'}, status=status.HTTP_403_FORBIDDEN)
+        notification.unread = False
+        notification.save()
+        serializer = self.get_serializer(notification)
+        return Response(serializer.data)
