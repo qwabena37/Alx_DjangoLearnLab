015a8a8f11ebb813d5aa14b33622ab4aa6b0d91e
James <qjaymce37@gmail.com>
2025-12-11 21:55:50 +0000

    set static files for deployment purposes in project/settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -140,7 +140,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Enable gzip and caching for Whitenoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 AUTH_USER_MODEL = 'accounts.User' #cutom user
 
 SECURE_BROWSER_XSS_FILTER = True
