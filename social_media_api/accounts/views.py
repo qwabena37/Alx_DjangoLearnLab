08d4e51f308f85cab333af0368d017ee05698298
James <qjaymce37@gmail.com>
2025-12-11 18:37:22 +0000

    modify accounts/views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializers import RegisterSerializer, LoginSerializer
+from .models import User
 
-# Create your views here.
+@api_view(['POST'])
+def register(request):
+    serializer = RegisterSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    user = serializer.save()
+
+    token, _ = Token.objects.get_or_create(user=user)
+
+    return Response({
+        "message": "User registered successfully",
+        "token": token.key
+    })
+
+
+@api_view(['POST'])
+def login(request):
+    serializer = LoginSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    user = serializer.validated_data
+
+    token, _ = Token.objects.get_or_create(user=user)
+
+    return Response({
+        "message": "Login successful",
+        "token": token.key
+    })
+
+
+@api_view(['GET'])
+def profile(request):
+    user = request.user
+    return Response({
+        "username": user.username,
+        "email": user.email,
+        "bio": user.bio,
+        "followers_count": user.followers.count()
+    })
